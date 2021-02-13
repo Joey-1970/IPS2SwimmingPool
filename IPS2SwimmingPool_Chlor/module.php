8720d46a9777aa66fa7cc017ff9aa82758c933bd
Joey-1970 <paeper@horburg.de>
2021-02-13 12:51:58 +0100

    Update module.php

--- IPS2SwimmingPool_Chlor/module.php
+++ IPS2SwimmingPool_Chlor/module.php
@@ -10,7 +10,11 @@ class IPS2SwimmingPool_Chlor extends IPSModule
 		$this->RegisterPropertyBoolean("Open", false);
 		$this->RegisterPropertyInteger("ORP_SensorID", 0); // ORP Sensor
 		$this->RegisterPropertyInteger("pH_SensorID", 0); // pH Sensor
+		$this->RegisterPropertyInteger("ORP_Target", 750);
 		$this->RegisterPropertyFloat("PoolVolume", 10);
+		$this->RegisterPropertyFloat("KP", 0.0);
+		$this->RegisterPropertyFloat("KD", 0.0);
+		$this->RegisterPropertyFloat("KI", 0.0);
 		$this->RegisterPropertyInteger("Timer_1", 60);
 		$this->RegisterTimer("Timer_1", 0, 'IPS2SwimmingPoolChlor_CalculateRedox($_IPS["TARGET"]);');
 		
@@ -21,6 +25,9 @@ class IPS2SwimmingPool_Chlor extends IPSModule
 		$this->RegisterVariableFloat("ORP", "ORP", "IPS2SwimmingPool.mV", 10);
 		$this->RegisterVariableFloat("pH", "pH", "~Liquid.pH.F", 20);
 		$this->RegisterVariableFloat("rH", "rH", "", 30);
+		
+		$this->RegisterVariableFloat("SumDeviation", "Summe Regelabweichungen", "IPS2SwimmingPool.mV", 70);
+		$this->RegisterVariableFloat("ActualDeviation", "Aktuelle Regelabweichung", "IPS2SwimmingPool.mV", 80);
 	}
  	
 	public function GetConfigurationForm() 
@@ -35,7 +42,12 @@ class IPS2SwimmingPool_Chlor extends IPSModule
 		$arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv");
 		$arrayElements[] = array("type" => "Label", "caption" => "_____________________________________________________________________________________________________");
 		$arrayElements[] = array("type" => "NumberSpinner", "name" => "Timer_1", "caption" => "Wiederholungszyklus in Sekunden (0 -> aus, 1 sek -> Minimum)", "suffix" => "Sekunden", "minimum" => 0);
-		
+		$arrayElements[] = array("type" => "Label", "caption" => "_____________________________________________________________________________________________________");
+		$arrayElements[] = array("type" => "NumberSpinner", "name" => "ORP_Target", "caption" => "Ziel-Redox-Wert", "suffix" => "mV", "minimum" => 0);
+		$arrayElements[] = array("type" => "NumberSpinner", "name" => "KP", "caption" => "Verstärkungsfaktor Proportionalregler", "digits" => 1);
+		$arrayElements[] = array("type" => "NumberSpinner", "name" => "KI", "caption" => "Verstärkungsfaktor Integralregler", "digits" => 1);
+		$arrayElements[] = array("type" => "NumberSpinner", "name" => "KD", "caption" => "Verstärkungsfaktor Differenzialregler", "digits" => 1);
+		$arrayElements[] = array("type" => "Label", "caption" => "_____________________________________________________________________________________________________");
 		$arrayElements[] = array("type" => "SelectVariable", "name" => "ORP_SensorID", "caption" => "ORP-Sensor-ID"); 
 		$arrayElements[] = array("type" => "SelectVariable", "name" => "pH_SensorID", "caption" => "pH-Sensor-ID"); 
 		
@@ -132,8 +144,55 @@ class IPS2SwimmingPool_Chlor extends IPSModule
 			$rH = 2 * $pH + (2 * $ORP) / 59.1;
 			
 			$this->SetValue("rH", $rH);
+			
+			$this->ControlLoop();
 		}
 		
+	}
+	
+	private function ControlLoop()
+	{
+		$this->SendDebug("ControlLoop", "Ausfuehrung", 0);
+		//Ta = Rechenschrittweite (Abtastzeit)
+		$Ta = Round( (time() - (int)$this->GetBuffer("LastTrigger")) / 60, 0);
+		//Schutzmechanismus falls Skript innerhalb einer Minute zweimal ausgeführt wird
+		$Ta = Max($Ta, 1);
+		
+		// Die vorherige Regelabweichung ermitteln
+		$ealt = $this->GetValue("ActualDeviation"); 
+		
+		//Aktuelle Regelabweichung bestimmen
+		$e = $this->GetValue("ORP_Target") - $this->GetValue("ORP");
+		
+		// Vorherige Regelabweichung durch jetzige ersetzen 
+		$this->SetValue("ActualDeviation", $e);
+		
+		//Die Summe aller vorherigen Regelabweichungen bestimmen
+		$esum = $this->GetValue("SumDeviation") + $e;
+		$this->SetValue("SumDeviation", $esum);
+		
+		// Zu kompensierende mV
+		$Compensate = $this->PID($this->ReadPropertyFloat("KP"), $this->ReadPropertyFloat("KI"), $this->ReadPropertyFloat("KD"), $e, $esum, $ealt, $Ta);
+
+		$this->SendDebug("ControlLoop", "Zu kompensierendes Defizit: ".$Compensate." mV", 0);
+	}
+	
+	// Berechnet nächsten Stellwert der Aktoren
+	private function PID($Kp, $Ki, $Kd, $e, $esum, $ealt, $Ta)
+	{
+		//e = aktuelle Reglerabweichung -> Soll-Ist
+		//ealt = vorherige Reglerabweichung
+		//esum = die Summe aller bisherigen Abweichungen e
+		//y = Antwort -> muss im Bereich zwischen 0-100 sein
+		//esum = esum + e
+		//y = Kp * e + Ki * Ta * esum + Kd * (e – ealt)/Ta
+		//ealt = e
+		//Kp = Verstärkungsfaktor Proportionalregler
+		//Ki = Verstärkungsfaktor Integralregler
+		//Kd = Verstärkungsfaktor Differenzialregler
+		// Die Berechnung des neuen Regelwertes
+		$y = ($Kp * $e + $Ki * $Ta * $esum + $Kd * ($e - $ealt) / $Ta);
+	return $y;
 	}
 	
 	private function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
